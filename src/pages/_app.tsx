3eb215dea2ab8b4a146430978ff3935d87f6de46
merdoyovski <merdoyovski@gmail.com>
2023-05-17 13:48:18 +0300

    update title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -32,7 +32,7 @@ export default function App(props: AppProps) {
   return (
     <>
       <Head>
-        <title>HollowDB Dashboard</title>
+        <title>HollowDB Auth</title>
         <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width"
